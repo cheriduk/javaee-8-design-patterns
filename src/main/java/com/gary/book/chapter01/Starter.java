1905111e11748076381786e66fcc4bfaf69c9482
dukang <15162678717@163.com>
2020-11-30 02:17:01 +0800

    java8设计模式学习记录 init

---
+++ src/main/java/com/gary/book/chapter01/Starter.java
@@ -0,0 +1,11 @@
+package com.gary.book.chapter01;
+
+public class Starter {
+
+//    Understanding design patterns
+//    Understanding the advantages of design patterns
+//    Defining the basic design patterns of the Java world
+//    Explaining enterprise patterns
+//    Explaining the difference between design patterns and enterprise patterns
+
+}
